b0658085120f5b6ecf284fefadc4a8fe72d25bb8
foliageh <mygazztop1@gmail.com>
2024-08-25 01:20:25 +0300

    add StudentService tests

--- README.md
+++ README.md
@@ -5,10 +5,12 @@
 Чтобы запустить тесты, достаточно установить [FTP Client](../../../FTPClient) 
 в локальный Maven репозиторий через `mvn install`, и затем в приложении с тестами выполнить `mvn test`.
 
-Альтернативный вариант – собрать jar через `mvn clean package` и запустить его как `java -jar target/FTPClient-tests`.  
+Альтернативный вариант – собрать jar через `mvn clean package` и запустить его как `java -jar target/FTPClient-tests.jar`.  
 _Размер jar подозрительно большой, но это всё из-за maven-assembly, до лучшего решения я пока не догадался._
 
 Чтобы тесты работали, необходимо также указать данные подключения к тестовому FTP-серверу в файле [testng.xml](src/test/resources/testng.xml).
+
+_Ошибки SLF4J при запуске тестов не влияют на работу программы, поэтому можно не обращать на них внимания, это проблема TestNG._
 
 ## Описание тестов
 ### FTPClientTests
@@ -16,8 +18,13 @@ _Размер jar подозрительно большой, но это всё 
 - `Disconnect_after_connection_successful` - проверяет успешное отключение после подключения
 - `Login_with_correct_credentials_successful` - проверяет успешную аутентификацию с правильными учетными данными
 - `Login_with_incorrect_credentials_fails_with_exception` - проверяет, что попытка аутентификации с неправильными учетными данными вызовет исключение
-- `Upload_textual_data_successful` - проверяет загрузку текстовых данных на сервер
-- `Download_textual_data_successful` - проверяет скачивание текстовых данных с сервера
+- `Upload_textual_data_successful` - проверяет успешную загрузку текстовых данных на сервер
+- `Download_textual_data_successful` - проверяет успешное скачивание текстовых данных с сервера
 - `Download_nonexistent_textual_data_fails_with_exception` - проверяет, что попытка скачивания несуществующих текстовых данных вызовет исключение
 
-Продолжение следует...
+### StudentServiceTests
+- `Create_student_successful` - проверяет успешное создание студента
+- `Create_multiple_students_successful` - проверяет успешное создание нескольких студентов
+- `Remove_student_successful` - проверяет успешное удаление студента
+
+Не покрыт тестами остался только UI. Без сторонних фреймворков не представляю возможным осуществить его модульное тестирование.
--- src/test/java/services/StudentServiceTests.java
+++ src/test/java/services/StudentServiceTests.java
@@ -1,8 +1,74 @@
 package services;
 
+import entities.Student;
 import ftp.FTPClient;
+import ftp.exceptions.FTPDataTransferException;
+import ftp.exceptions.FTPException;
+import ftp.exceptions.FTPIllegalReplyException;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class StudentServiceTests {
-    StudentService studentService = new StudentService(new FTPClient());
+    private static final String TEST_STUDENT_DATA_FTP_FILEPATH = "students_test.json";
+    private StudentService studentService;
+    private FTPClient ftpClient;
 
+    @BeforeMethod
+    @Parameters({"serverIp", "username", "password"})
+    public void setUp(String serverIp, String username, String password) throws FTPIllegalReplyException, IOException, FTPException, FTPDataTransferException {
+        // Connect to the FTP server and login
+        ftpClient = new FTPClient();
+        ftpClient.connect(serverIp);
+        ftpClient.login(username, password);
+
+        // Create an empty student data file
+        InputStream emptyInputStream = new ByteArrayInputStream(new byte[0]);
+        ftpClient.uploadTextualData(TEST_STUDENT_DATA_FTP_FILEPATH, emptyInputStream);
+
+        studentService = new StudentService(ftpClient);
+    }
+
+    @AfterMethod
+    public void tearDown() throws FTPIllegalReplyException, FTPDataTransferException, IOException, FTPException {
+        // Delete the contents of the student data file
+        InputStream emptyInputStream = new ByteArrayInputStream(new byte[0]);
+        ftpClient.uploadTextualData(TEST_STUDENT_DATA_FTP_FILEPATH, emptyInputStream);
+
+        // Disconnect from the FTP server
+        try {
+            ftpClient.disconnect();
+        } catch (IllegalStateException ignored) {
+        }
+    }
+
+    @Test
+    public void Create_student_successful() throws Exception {
+        Student student = studentService.createStudent("Mario");
+        studentService.downloadStudentData();
+        Assert.assertTrue(studentService.getLocalStudentData().containsKey(student.getId()));
+    }
+
+    @Test
+    public void Create_multiple_students_successful() throws Exception {
+        Student student1 = studentService.createStudent("Mario");
+        Student student2 = studentService.createStudent("Luigi");
+        studentService.downloadStudentData();
+        Assert.assertTrue(studentService.getLocalStudentData().containsKey(student1.getId()));
+        Assert.assertTrue(studentService.getLocalStudentData().containsKey(student2.getId()));
+    }
+
+    @Test
+    public void Remove_student_successful() throws Exception {
+        Student student = studentService.createStudent("Mario");
+        studentService.removeStudent(student.getId());
+        studentService.downloadStudentData();
+        Assert.assertFalse(studentService.getLocalStudentData().containsKey(student.getId()));
+    }
 }
